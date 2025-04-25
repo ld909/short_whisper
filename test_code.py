f2c1efe0f807554794d550452a5cccf7dec0ba1f
liu <dhliu.ai@gmail.com>
2025-04-25 22:46:45 +0800

    remove secrets

--- test_code.py
+++ test_code.py
@@ -19,32 +19,3 @@ import os
 #     print("成功取消clash环境proxy...")
 
 
-def test_code():
-    pipeline = Pipeline.from_pretrained(
-        "pyannote/speaker-diarization-3.1",
-        use_auth_token="YOUR_TOKEN_PLACEHOLDER",
-    )
-
-    # send pipeline to GPU (when available)
-    import torch
-
-    pipeline.to(torch.device("cuda"))
-
-    # apply pretrained pipeline
-    diarization = pipeline("./tt.mp3", num_speakers=2)
-
-    # print the result
-    for turn, _, speaker in diarization.itertracks(yield_label=True):
-        print(f"start={turn.start:.1f}s stop={turn.end:.1f}s speaker_{speaker}")
-    # start=0.2s stop=1.5s speaker_0
-    # start=1.8s stop=3.9s speaker_1
-    # start=4.2s stop=5.7s speaker_0
-    # ...
-
-
-if __name__ == "__main__":
-    # set_clash_proxy()
-    # try to ping google.com for 10 seconds
-    # os.system("ping -c 10 google.com")
-    test_code()
-    # unset_clash_proxy()
